2580995ff3bc1ccb1762b2716f451d49257a7fc1
Sayani Biswas <sayanibiswas.16@nshm.edu.in>
2026-03-11 12:14:59 +0530

    fix API url

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
-// db.js
 import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // required for Neon
+ connectionString: process.env.DATABASE_URL,
+ ssl: process.env.NODE_ENV === "production"
+  ? { rejectUnauthorized: false }
+  : false
 });
 
 export default pool;
